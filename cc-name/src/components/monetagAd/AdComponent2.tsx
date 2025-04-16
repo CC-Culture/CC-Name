1f5013169ac60fe534314cbd620ad6c1a7fb1ed7
PushedChris <ciny@vip.qq.com>
2025-04-16 16:23:53 +0800

    feature:fix adds

--- cc-name/src/components/monetagAd/AdComponent2.tsx
+++ cc-name/src/components/monetagAd/AdComponent2.tsx
@@ -6,7 +6,8 @@ interface AdComponentProps {
   className?: string;
 }
 
-const localScript = `(function(d,z,s){s.src='https://'+d+'/401/'+z;try{(document.body||document.documentElement).appendChild(s)}catch(e){}})('groleegni.net',9219445,document.createElement('script'))`;
+const localScript = `(function(d,z,s){s.src='https://'+d+'/400/'+z;try{(document.body||document.documentElement).appendChild(s)}catch(e){}})('vemtoutcheeg.com',9219562,document.createElement('script'))`;
+
 const AdComponent2: React.FC<AdComponentProps> = ({ className = "" }) => {
   useEffect(() => {
     const script = document.createElement("script");
